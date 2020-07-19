06be7da54e4ac29ac87c6e68626bbc3226217f4e
akfreed <alexkgray@gmail.com>
2020-07-19 13:32:10 -0700

    Added Apache 2.0 LICENSE and license headers.

--- windows/UdpSocket.h
+++ windows/UdpSocket.h
@@ -1,6 +1,19 @@
-//============================================================================
-// Copyright (c) 2018 Alexander Freed
+// ==================================================================
+// Copyright 2018 Alexander K. Freed
 //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ==================================================================
+
 // Contains the declaration for UdpSocket
 // In the current implementation, a UdpSocket either sets its recipient at
 // instantiation, or the first sender it reads from will be locked in as the
@@ -11,7 +24,7 @@
 //
 // The copy operations are deleted, so standard containers will used move 
 // operations, however the strong exception guarantee is lost.
-//============================================================================
+
 #pragma once
 
 #include "SocketIncludes.h"
