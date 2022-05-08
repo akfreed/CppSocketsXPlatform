d0c53f5c7e8894972ddd04557544277b50f565c3
akfreed <alexkgray@gmail.com>
2022-05-08 03:11:20 -0700

    copyrights

--- UnitTest/TestGlobals.h
+++ UnitTest/TestGlobals.h
@@ -1,5 +1,5 @@
 // ==================================================================
-// Copyright 2018, 2021 Alexander K. Freed
+// Copyright 2018-2022 Alexander K. Freed
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -24,7 +24,7 @@ struct TestGlobals
 {
     static char constexpr localhost[] = "127.0.0.1";
     static uint16_t constexpr port = 11111;
-    static uint16_t constexpr  port2 = 11112;
+    static uint16_t constexpr port2 = 11112;
 };
 
 } } }
